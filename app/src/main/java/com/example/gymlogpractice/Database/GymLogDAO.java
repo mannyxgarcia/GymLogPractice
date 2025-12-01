0bae4d3b17092b2fbf35e6873449a7131e94daae
Manny Garcia <mannyalgarcia@gmail.com>
2025-12-01 09:47:25 -0800

    Added database

---
+++ app/src/main/java/com/example/gymlogpractice/Database/GymLogDAO.java
@@ -0,0 +1,23 @@
+package com.example.gymlogpractice.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.gymlogpractice.Database.entities.GymLog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface GymLogDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(GymLog gymlog);
+
+    @Query("SELECT * from " + GymLogDatabase.GYM_LOG_TABLE)
+    ArrayList<GymLog> getAllRecords();
+
+
+
+}
